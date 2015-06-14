4fc53a3689e9e0460436b28b52da21d42ae2fcd9
Leigh Zhu <i@zhuli.me>
2015-06-14 14:17:42 +0800

    update nvm.sh

--- apps/nvm.sh
+++ apps/nvm.sh
@@ -10,15 +10,15 @@ fi
 source ~/.nvm/nvm.sh
 
 # install node
-echo "Installing: node 0.10.x"
-nvm install 0.10
+echo "Installing: node 0.12.x"
+nvm install 0.12
 
-echo "Installing: node 0.11.x"
-nvm install 0.11
+echo "Installing: iojs"
+nvm install iojs
 
-nvm use 0.11
+nvm use iojs
 
 echo "set default node version to 0.11.x"
-nvm alias default 0.11
+nvm alias default iojs
 
 exit 0
